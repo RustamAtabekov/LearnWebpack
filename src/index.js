d218e89b678f970460e96f8e3af6e6b3393f3b4d
Rustam Atabekov <atabekovrustam@gmail.com>
2022-06-29 20:41:52 +0300

    Add to webpack.config.js sass-loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import AppService from "./modules/app.service";
 import { config } from "./modules/config";
 import "./modules/header.component";
 import "./css/index.css";
+import "./scss/index.scss";
 
 console.log("Config: ", config.key);
 
