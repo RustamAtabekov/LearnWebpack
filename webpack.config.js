40aa516bd4061f777e5748629d2d9a29a468e1b3
Rustam Atabekov <atabekovrustam@gmail.com>
2022-06-29 17:29:29 +0300

    Install terser-webpack-plugin, css-minimizer-webpack-plugin, and turn on optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -23,5 +25,10 @@ module.exports = {
     rules: [
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
     ],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
 };
