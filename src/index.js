c9b44f2170fbc6cd1415be1e59183593328b2913
Rustam Atabekov <atabekovrustam@gmail.com>
2022-06-29 11:54:24 +0300

    Work with dev and production modes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+import AppService from './modules/app.service';
+import {config} from './modules/config';
+import './modules/header.component';
+
+console.log('Config: ', config.key);
+
+const service = new AppService('Hello world');
+service.log();
